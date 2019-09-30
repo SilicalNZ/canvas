3b5f1e72572fe1ba821f3b30cfcd40fa4aba9aab
A. Randell <SilicalNZ@gmail.com>
2019-09-30 20:05:05 +1300

    Added example: rearrange_segments

--- EXAMPLES/sort_an_image.py
+++ EXAMPLES/sort_an_image.py
@@ -18,3 +18,17 @@ def open_and_save_image(func):
 def rearrange(c: canvas.Canvas):
     c.rearrange(canvas.tools.yiq) # Sorts by luminosity
     return c
+
+@open_and_save_image
+def rearrange_segments(c: canvas.Canvas):
+    controller = canvas.CanvasController(c)
+    # A rectangle of width = default, length = 20
+    width, length = c.width, c.length // 20
+    # Splits the canvas into those rectangles
+    # and returns a generator to iterate through
+    for i in controller.fragment((width, length)):
+        # Sort each rectangle by luminosity
+        i.rearrange(canvas.tools.yiq)
+    # Save all changes
+    controller.unscope_all()
+    return c

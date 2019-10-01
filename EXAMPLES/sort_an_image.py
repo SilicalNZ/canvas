c6f9c75474e1efa379479ef4cbafd0831ae07d66
A. Randell <SilicalNZ@gmail.com>
2019-10-02 03:37:25 +1300

    Moved comment

--- EXAMPLES/sort_an_image.py
+++ EXAMPLES/sort_an_image.py
@@ -16,7 +16,8 @@ def open_and_save_image(func):
 
 @open_and_save_image
 def rearrange(c: canvas.Canvas):
-    c.rearrange(canvas.tools.yiq) # Sorts by luminosity
+    # Sorts by luminosity
+    c.rearrange(canvas.tools.yiq)
     return c
 
 @open_and_save_image

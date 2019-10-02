b94eeaeb10eb6461e30dcef0b1ada21422fbe1bb
A. Randell <SilicalNZ@gmail.com>
2019-10-03 08:42:50 +1300

    Separated pillow functionality into unique file

--- canvas/__init__.py
+++ canvas/__init__.py
@@ -1,4 +1,12 @@
-from .canvas import PILCanvas as Canvas
+try:
+    from PIL import Image
+except ImportError:
+    import warnings
+    warnings.warn('Pillow failed to import. PIL-specific functionality will be unavailable.')
+    from .canvas import Canvas
+else:
+    from .pillow_extension import PILCanvas as Canvas
+
 from .canvas import SimpleCanvasAbstraction as CanvasApplier
 from .canvas import CanvasController
 from .canvas import CanvasMerger
--- canvas/canvas.py
+++ canvas/canvas.py
@@ -6,8 +6,6 @@ from typing import Type
 
 from .common import *
 
-from PIL import Image
-
 
 class SizeInfo:
     def __init__(self, size):
@@ -164,24 +162,6 @@ class Canvas(NoneIsImportantTuple, SizeInfo):
                 try: self[(x, y)] = width
                 except IndexError:
                     pass
-
-
-class PILCanvas(canvas.Canvas):
-    @classmethod
-    def from_PIL(cls, im: Image):
-        return cls(im.getdata(), im.size)
-
-    @classmethod
-    def from_image(cls, fp, mode):
-        return cls.from_PIL(Image.open(fp).convert(mode))
-
-    def as_PIL(self):
-        im = Image.new('RGB', self.size, 'black')
-        im.putdata(self.data)
-        return im
-
-    def save(self, fp, format=None, **params):
-        self.as_PIL().save(fp, format=None, **params)
 
 
 class ClosedCanvas(Canvas, NoneIsImmutableTuple):
